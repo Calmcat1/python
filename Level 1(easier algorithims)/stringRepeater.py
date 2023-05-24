d78bff4e3befe0647f76d90a7a9fa4ddf7666b1c
Lee <lgitonga101@gmail.com>
2023-05-24 10:32:01 +0300

    modified:   Level 1(easier algorithims)/max_no_in_arr_and_min_no_in_arr.py   modified:   Level 1(easier algorithims)/stringRepeater.py

--- Level 1(easier algorithims)/stringRepeater.py
+++ Level 1(easier algorithims)/stringRepeater.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Level 1(easier algorithims)/stringRepeater.py
 def repeat_str(repeat, string):
     output = ""
     for i in range(repeat+1):
@@ -9,16 +8,4 @@ print(repeat_str(5,"hello"))
         
 
 
-=======
-def repeat_str(repeat, string):
-    output = ""
-    for i in range(repeat+1):
-        output += string
-    return output
-
-print(repeat_str(5,"hello"))
-        
-
-
->>>>>>> ea1a98c2c739b483b898695f79d1d5ec2d753a1d:stringRepeater.py
 #repeats string for the specified amounts of times put as the first parameter
