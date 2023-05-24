511514d23f886ac227d25bad8bc1bb505a62e16f
Lee <lgitonga101@gmail.com>
2023-05-24 10:26:19 +0300

    deleted:    filterList_algo.py 	deleted:    find_the_smallest_integer.py   deleted:    integer_2_string.py 	deleted:    mapsA.py 	deleted:    match_first&last_char.py 	deleted:    perfectSquare.py 	deleted:    string_2_integer.py

--- integer_2_string.py
+++
@@ -1,3 +0,0 @@
-def number_to_string(num):
-    stingifiedNum = str(num)
-    return stingifiedNum
--- perfectSquare.py
+++
@@ -1,9 +0,0 @@
-def is_square(n):
-    if type(n **(1/2)) == int:
-      return "True"
-    else: 
-      return "False"
-
-
-print(is_square(25))
-
