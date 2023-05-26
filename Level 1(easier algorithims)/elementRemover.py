0c4f73ad45824030aac18751a193c2b582e9ca9f
Lee <lgitonga101@gmail.com>
2023-05-26 13:25:13 +0300

    new file:   Level 1(easier algorithims)/elementRemover.py

---
+++ Level 1(easier algorithims)/elementRemover.py
@@ -0,0 +1,10 @@
+def remove_every_other(my_list):
+    my_arr = list(my_list)
+    n  = len(my_arr)
+    for i in range(n):
+      my_arr.remove()
+    return my_arr
+
+
+print(remove_every_other([1,3,5,7,9,11,13]))
+        
