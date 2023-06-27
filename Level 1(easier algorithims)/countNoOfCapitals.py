c3451ac11def99e837549b99149d51e77c2a74e5
Lee <lgitonga101@gmail.com>
2023-06-27 11:25:03 +0300

    new file:   Level 1(easier algorithims)/countNoOfCapitals.py 	new file:   Level 1(easier algorithims)/sortString.py
     Changes not staged for commit:
    	modified:   Level 1(easier algorithims)/detectPangram.py
    	modified:   Level 1(easier algorithims)/sorting_algo.py
    
     Untracked files:
    	Level 1(easier algorithims)/TheWave.py
    	Level 1(easier algorithims)/arithmetic_algo.py
    	Level 1(easier algorithims)/arrayReverser.py
    	Level 1(easier algorithims)/basicMathOp.py
    	Level 1(easier algorithims)/countSmileyFaces.py
    	Level 1(easier algorithims)/divisorNumberTest.py
    	Level 1(easier algorithims)/friendOrFoe.py
    	Level 1(easier algorithims)/is_palidrome.py
    	Level 1(easier algorithims)/isograms.py
    	Level 1(easier algorithims)/line_Numbering.py
    	Level 1(easier algorithims)/onesAndzeros.py
    	Level 1(easier algorithims)/queueTimeTotal.py
    	Level 1(easier algorithims)/removeAfter#.py
    	Level 1(easier algorithims)/removingEveryN+1Elements.py
    	Level 1(easier algorithims)/returnConcatenatedSquare.py
    	Level 1(easier algorithims)/reverseString.py
    	Level 1(easier algorithims)/time_After_Midnight_in_ms.py
    	Level 1(easier algorithims)/unique_elements.py
    	Level 1(easier algorithims)/visualStudio.py
    	Level 1(easier algorithims)/vowelRemover.py

---
+++ Level 1(easier algorithims)/countNoOfCapitals.py
@@ -0,0 +1,2 @@
+def countable(string):
+    return 
---
+++ Level 1(easier algorithims)/sortString.py
@@ -0,0 +1,12 @@
+def sort_by_length(arr):
+    strLen = []
+    for string in arr:
+        n = len(string)
+        strLen.append(n)
+        strLen.sort()
+
+    
+
+
+
+          
